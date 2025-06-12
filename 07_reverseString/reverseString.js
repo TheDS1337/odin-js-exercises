e7ae0c7465e610cf344e9d64fa5ae3c216ab118f
DS <aboussoubel@gmail.com>
2025-06-12 10:54:49 +0700

    Solve 7th exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // Strings are immutable, so we work with arrays
+    let arr = Array.from(str);
+    let reverseArr = arr.toReversed();
+/*
+    for( let i = arr.length - 1; i >= 0; i-- )
+        reverseArr.push(arr[i]);
+*/
+    return reverseArr.join('');
 };
 
 // Do not edit below this line
