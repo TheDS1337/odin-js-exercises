3b9c88ff89736d18f77035c7d251494997e5bdc5
DS <aboussoubel@gmail.com>
2025-06-12 10:39:39 +0700

    Solve 6th exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if( n < 0 )
+        return 'ERROR';
 
+    // Dont even bother
+    if( str === '' || n === 0 )
+        return '';
+
+    let newStr = '';
+
+    for( let i = 1; i <= n; i++ )
+        newStr += str;
+
+    return newStr;
 };
 
 // Do not edit below this line
