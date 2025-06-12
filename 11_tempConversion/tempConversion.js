5a2e8e9cea13ade5424984dddc466526fda810fb
DS <aboussoubel@gmail.com>
2025-06-12 11:26:46 +0700

    Solve 11th exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const roundUpToDecimal = function(num, dec = 1) {
+  let expOfBase = Math.pow(10, dec);
+  return Math.round(num * expOfBase) / expOfBase;
+}
+
+const convertToCelsius = function(temp) {
+  return roundUpToDecimal((temp - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return roundUpToDecimal(9 * temp / 5 + 32);
 };
 
 // Do not edit below this line
