b0029261aa84618949c2ae2afd077a5aa1161bbc
DS <aboussoubel@gmail.com>
2025-06-12 11:02:43 +0700

    Solve 8th exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for( const arg of args )
+        arr = arr.filter((val) => val !== arg);
+
+    return arr;
 };
 
 // Do not edit below this line
