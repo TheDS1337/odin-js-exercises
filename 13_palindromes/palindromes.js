0d5f2bcf55a4138b5e66bff12ab234e5017a2a74
DS <aboussoubel@gmail.com>
2025-06-14 13:39:13 +0700

    Solve 13th exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,34 @@
-const palindromes = function () {
+const isAlphabet = (c) => c.match(/[a-z]/i) !== null;
+const isNumber = (c) => !isNaN(parseInt(c));
 
+const palindromes = function (str) {
+    let len = str.length;
+    let mid = Math.ceil(len / 2);
+
+    str = str.trim();
+    str = str.toLowerCase();
+    
+    for( let i = 0, j = len - 1; i < mid; i++ ) {
+        let a = str[i];
+
+        if( !isAlphabet(a) && !isNumber(a) )
+            continue;
+
+        for( ; j > i; j-- ) {
+            let b = str[j];
+
+            if( !isAlphabet(b) && !isNumber(b) )
+                continue;
+
+            if( b === a ) {
+                j--;
+                break;
+            } else                
+                return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
